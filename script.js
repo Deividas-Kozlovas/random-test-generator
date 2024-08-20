96362698ee1e7e15f35611889e012cf09c7d8e87
Deiv-Dev <kozlovas.deividas@gmail.com>
2024-08-20 16:19:39 +0300

    added test list for generating test, fixed logict to work with testData.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 import { testList } from './testData.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const textBox = document.getElementsByClassName(".box__text");
-    const roteateButton = document.getElementsByClassName(".box__rotate");
+    const textBox = document.getElementsByClassName("box__text")[0];
+    const roteateButton = document.getElementsByClassName("box__rotate")[0];
     const randomLoopCount = Math.floor(Math.random() * 100);
 
     roteateButton.addEventListener('click', () => {
         let count = 0;
         function rotate(randomLoopCount, count){
             if(randomLoopCount > count){
-                const randomIndex = Math.floor(Math.random() * testList[randomLoopCount])
-                textBox.innerHtml = `<p>${testList[randomIndex]}</p>`;
+                const randomIndex = Math.floor(Math.random() * testList.length);
+                textBox.innerHTML = `<p>${testList[randomIndex]}</p>`;
             }
             setTimeout(() => {
                 rotate(randomLoopCount, count + 1);
