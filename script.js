555ad92a823d93a971641c39accc0a4abeba4e45
Deividas-Kozlovas <kozlovas.deividas@gmail.com>
2025-07-13 20:52:55 +0300

    fix

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-import { testList } from './testData.js';
+import { testList, agentDescription } from "./testData.js";
 
-document.addEventListener('DOMContentLoaded', () => {
-    const textBox = document.getElementsByClassName("box__text")[0];
-    const roteateButton = document.getElementsByClassName("box__rotate")[0];
-    const randomLoopCount = Math.floor(Math.random() * 100);
+document.addEventListener("DOMContentLoaded", () => {
+  const textBox = document.getElementsByClassName("box__text")[0];
+  const roteateButton = document.getElementsByClassName("box__rotate")[0];
+  const randomLoopCount = Math.floor(Math.random() * 100);
 
-    roteateButton.addEventListener('click', () => {
-        let count = 0;
-        function rotate(randomLoopCount, count){
-            if(randomLoopCount > count){
-                const randomIndex = Math.floor(Math.random() * testList.length);
-                textBox.innerHTML = `<p>${testList[randomIndex]}</p>`;
-                setTimeout(() => {
-                    rotate(randomLoopCount, count + 1);
-                }, 20)
-            }
-        }
-        rotate(randomLoopCount, count);
-    });
+  roteateButton.addEventListener("click", () => {
+    let count = 0;
+    function rotate(randomLoopCount, count) {
+      if (randomLoopCount > count) {
+        const randomIndex = Math.floor(Math.random() * testList.length);
+        textBox.innerHTML = `<p>${agentDescription} ${testList[randomIndex]}</p>`;
+        setTimeout(() => {
+          rotate(randomLoopCount, count + 1);
+        }, 20);
+      }
+    }
+    rotate(randomLoopCount, count);
+  });
 });
