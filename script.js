bb80d76f01f8e6e8ba15a79b9cd3a20b9633fd85
Deividas Kozlovas <Kozlovas.Deividas@gmail.com>
2025-07-25 18:50:36 +0300

    refactor

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 import { testList, agentDescription } from "./testData.js";
 
+// Constants for animation
+const MAX_ANIMATION_CYCLES = 100;
+const ANIMATION_DELAY_MS = 20;
+
 document.addEventListener("DOMContentLoaded", () => {
   const textBox = document.getElementsByClassName("box__text")[0];
-  const roteateButton = document.getElementsByClassName("box__rotate")[0];
-  const randomLoopCount = Math.floor(Math.random() * 100);
+  const rotateButton = document.getElementsByClassName("box__rotate")[0];
 
-  roteateButton.addEventListener("click", () => {
-    let count = 0;
-    function rotate(randomLoopCount, count) {
-      if (randomLoopCount > count) {
-        const randomIndex = Math.floor(Math.random() * testList.length);
-        textBox.innerHTML = `<p>${agentDescription} ${testList[randomIndex]}</p>`;
-        setTimeout(() => {
-          rotate(randomLoopCount, count + 1);
-        }, 20);
-      }
+  // Rotate function for animating the random test display
+  function rotateText(cycles, count = 0) {
+    if (count < cycles) {
+      const randomIndex = Math.floor(Math.random() * testList.length);
+      textBox.innerHTML = `<p>${agentDescription} ${testList[randomIndex]}</p>`;
+      setTimeout(() => {
+        rotateText(cycles, count + 1);
+      }, ANIMATION_DELAY_MS);
+    } else {
+      // Re-enable the button after animation
+      rotateButton.disabled = false;
     }
-    rotate(randomLoopCount, count);
+  }
+
+  rotateButton.addEventListener("click", () => {
+    // Prevent multiple animations at once
+    rotateButton.disabled = true;
+    const animationCycles = Math.floor(Math.random() * MAX_ANIMATION_CYCLES);
+    rotateText(animationCycles);
   });
 });
